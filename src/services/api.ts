02aa804fe4a1322ba2e0a88ec5bfe9ebbc08dad1
Maurício Bosi Schmidt <mauriciobosi@gmail.com>
2021-11-17 19:32:58 -0300

    semana09-final

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3334'
+  baseURL: process.env.NODE_ENV === 'production' 
+    ?'https://mas-backend.herokuapp.com'
+    :'http://localhost:3334',
 });
 
 export default api;
